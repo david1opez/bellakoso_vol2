5f9346a5007119b8c9afb98322ed553dd27b8762
Andres De la Garza <andy562.dlg@gmail.com>
2023-04-12 17:17:34 -0600

    "turning correction"

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
-	Translate(10,10);
+	Translate(10,0);
 }
 
 /**
@@ -59,7 +59,7 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	Translate(30, 30);
+	Translate(30,30);
 
 	while (true) {
 		Drive();
--- src/system/autonomous/Translate.cpp
+++ src/system/autonomous/Translate.cpp
@@ -40,7 +40,7 @@ void MoveBackwards(int speed) {
     Left_Back_Bottom_Wheel.move(-speed);
 }
 
-void Stop(int speed) {
+void Stop() {
     Right_Front_Wheel.move(0);
     Right_Back_Top_Wheel.move(0);
     Right_Back_Bottom_Wheel.move(0);
@@ -57,10 +57,10 @@ void Translate(double x, double y,std::optional<std::string> subsystem, std::opt
     float currentTranslateAngle = GetAngle();
 
     // Calculate the angle between the current position and the target position
-    float targetAngle = atan2(y - currentTranslateY, x - currentTranslateX) * 180 / M_PI;
+    float targetAngle = atan2(x - currentTranslateX, y - currentTranslateY) * 180 / M_PI;
 
     // Calculate the distance between the current position and the target position
-    float distance = sqrt(pow(x - currentTranslateY, 2) + pow(y - currentTranslateY, 2));
+    float distance = sqrt(pow(x - currentTranslateX, 2) + pow(y - currentTranslateY, 2));
 
     auto updatePosition = [&]() {
         // Update the current position of the robot
@@ -103,7 +103,7 @@ void Translate(double x, double y,std::optional<std::string> subsystem, std::opt
             } else {
                 if(distance > 0.5) {
                     // Move forward
-                    // MoveForwards(TranslatePID(x,y,1));
+                    MoveForwards(TranslatePID(x,y,1));
                 } else {
                     if(currentTranslateAngle > angle.value_or(0) + 1) {
                         // Turn left
@@ -133,9 +133,19 @@ void Translate(double x, double y,std::optional<std::string> subsystem, std::opt
 
             if(abs(angleDifference) > errorMargin) {
                 if(angleDifference > 0) {
-                    TurnRight(30);
+                    while (angleDifference > 0 )
+                    {
+                        currentTranslateAngle = GetAngle();
+                        angleDifference = targetAngle - currentTranslateAngle;
+                        TurnRight(40);
+                    }
                 } else if (angleDifference < 0) {
-                    TurnLeft(30);
+                    while (angleDifference > 0)
+                    {
+                        currentTranslateAngle = GetAngle();
+                        angleDifference = targetAngle - currentTranslateAngle;
+                        TurnLeft(30);
+                    }
                 }
             }
             else {
@@ -151,6 +161,8 @@ void Translate(double x, double y,std::optional<std::string> subsystem, std::opt
                         // Turn right
                     } else {
                         // Stop
+                        Stop();
+                        break;
                     }
                 }
             }
