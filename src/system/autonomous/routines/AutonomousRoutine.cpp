30cb836ba78a69f02fc071ea7228a950073e3341
Andres De la Garza <andy562.dlg@gmail.com>
2023-04-20 17:24:17 -0600

    "."

--- src/system/autonomous/routines/AutonomousRoutine.cpp
+++ src/system/autonomous/routines/AutonomousRoutine.cpp
@@ -4,83 +4,28 @@ void AutonomousRoutine(Status Components_Status) {
     // Move Roller
     TranslateInches({
         .componentsStatus = Components_Status,
-        .PID = false,
-        .distance = 3,
-        .reverse = true,
-        .translatePower = 15,
-        .timeout = 70,
-        .subsystem = "roller",
-        .subsystemTimeout = 160,
-        .backwards = true,
+        .PID = true,
+        .distance = 14.75,
+        .reverse = false,
+        .angle = 0,
+        .translatePower = 15, //change later
+        .timeout = 5000,
+        .subsystem = "intaker",
+        .subsystemTimeout = 7000,
     });
 
     TranslateInches({
+        
         .PID = false,
-        .distance = 3,
-        .timeout = 70
+        .distance = 18,
+        .reverse = false,
+        .angle = -45,
+        .translatePower = 10, //change later
+        .timeout = 5000,
+        .subsystem = "intaker",
+        .subsystemTimeout = 7000,
     });
 
-    // Pick first disc
-    // TranslateInches({
-    //     .distance = 8.5,
-    //     .angle = -40.2,
-    //     .timeout = 3000,
-    //     .subsystem = "intaker",
-    //     .subsystemTimeout = 1500,
-    // });
 
-
-
-    // Move backwards to turn
-    // TranslateInches({
-    //     .distance = 3,
-    //     .reverse = true,
-    //     .angle = -40,
-    //     .timeout = 150,
-    //     .subsystem = "intaker",
-    //     .subsystemTimeout = 1000,
-    // });
-
-    // Charge Flywheel
-    // ActivateSystem("flywheel", 12000, true);
-
-    // Move to first shooting position
-    // TranslateInches(20, false, 35, 1, 7000, "intaker", 1000);
-    // TranslateInches({
-    //     .distance = 20,
-    //     .angle = 35,
-    //     .timeout = 7000,
-    //     .subsystem = "intaker",
-    //     .subsystemTimeout = 1000,
-    // });
-
-    // TranslateInches(12, false, -44, 1, 2000, "", 100);
-    // TranslateInches({
-    //     .distance = 12,
-    //     .angle = -44,
-    //     .timeout = 7000,
-    //     .subsystem = "",
-    //     .subsystemTimeout = 1000,
-    // });
-
-
-    // Shoot 3 discs without charging the flywheel
-    // ActivateSystem("flywheel 3", 11800, true);
-    //ActivateSystem({
-    //    .system = "flywheel",
-    //    .discs = true
-    //});
     
-
-    // Turn off flywheel
-    // ActivateSystem("flywheel", 0, false);
-
-    // Move to pick up stacked discs
-
-    // Pick up stacked discs
-    // TranslateInches(10, false, 35, 1, 7000, "intaker", 3000);
-
-    // Move to second shooting position
-
-    // Shoot 3 discs
 }
--- src/system/control/Drive.cpp
+++ src/system/control/Drive.cpp
@@ -4,9 +4,9 @@ void Drive() {
     int power = Controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
     int turn = Controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
 
-    Right_Front_Wheel.move(power - turn);
-    Right_Back_Top_Wheel.move(power - turn);
-    Right_Back_Bottom_Wheel.move(power - turn);
+    Right_Front_Wheel.move(-1.5*(power - turn));//change back for driver
+    Right_Back_Top_Wheel.move(-1.5*(power - turn));
+    Right_Back_Bottom_Wheel.move(-1.5*(power - turn));
 
     Left_Front_Wheel.move(power + turn);
     Left_Back_Top_Wheel.move(power + turn);
