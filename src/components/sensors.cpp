8c34bdf85e7217e4cc5b77680788006eac1045a1
david <degl0412@gmail.com>
2023-04-21 10:06:40 -0600

    Add Rotation Sensor

--- src/components/sensors.cpp
+++ src/components/sensors.cpp
@@ -3,3 +3,5 @@
 pros::ADIEncoder Y_Axis_Encoder(Y_AXIS_ENCODER_TOP_PORT, Y_AXIS_ENCODER_BOTTOM_PORT, false);
 
 pros::IMU Inertial_Sensor(INERTIAL_SENSOR_PORT);
+
+pros::Rotation Rotation_Sensor(ROTATION_SENSOR_PORT);
