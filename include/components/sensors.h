1bc705c7e14415521d1cfd3f9092ce8f393dfedf
david <degl0412@gmail.com>
2023-04-21 10:06:07 -0600

    Defined rotation sensor

--- include/components/sensors.h
+++ include/components/sensors.h
@@ -12,4 +12,6 @@ extern pros::ADIEncoder Y_Axis_Encoder; // Y Axis Encoder
 
 extern pros::IMU Inertial_Sensor; // Inertial Sensor
 
+extern pros::Rotation Rotation_Sensor; // Rotation Sensor
+
 #endif // __SENSORS_H__
