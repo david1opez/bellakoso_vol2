f01a2eef16aa513946d4c4bdbd69fb6dee17d819
Andres De la Garza <andy562.dlg@gmail.com>
2023-04-23 17:47:09 -0600

    .

--- src/system/positioning/BackupSystems.cpp
+++ src/system/positioning/BackupSystems.cpp
@@ -16,7 +16,7 @@ double GetBackupDistance() {
 
 double GetBackupAngle() {
     double Left_Side_Encoders_Value = (Left_Back_Bottom_Wheel.get_position() + Left_Back_Top_Wheel.get_position() + Left_Front_Wheel.get_position()) / 3;
-    double Right_Side_Encoders_Value = (Right_Back_Bottom_Wheel.get_position() + Right_Back_Top_Wheel.get_position() + Right_Front_Wheel.get_position()) / 3;
+    double Right_Side_Encoders_Value = (-Right_Back_Bottom_Wheel.get_position() -Right_Back_Top_Wheel.get_position() ) / 2;
 
     double Left_Side_Distance = Left_Side_Encoders_Value * 3.125 * M_PI / 360.0;
     double Right_Side_Distance = Right_Side_Encoders_Value * 3.125 * M_PI / 360.0;
