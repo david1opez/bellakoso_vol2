7ccdab6f2f32c7056dc7cc3cf50c9fdde5dce1e4
david <degl0412@gmail.com>
2023-04-24 19:41:36 -0600

    dfghjk

--- src/system/control/HandleExpansion.cpp
+++ src/system/control/HandleExpansion.cpp
@@ -4,6 +4,6 @@
 
 void HandleExpansion() {
     if(Controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A) == 1) {
-        Expansion.move_absolute(200, 100);
+        Expansion.move_absolute(300, 100);
     }
 }
